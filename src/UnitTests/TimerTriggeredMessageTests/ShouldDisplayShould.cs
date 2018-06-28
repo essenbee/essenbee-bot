00b2bc51f81dd47e3877a24895bd6f1d6da04769
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-06-28 19:15:57 +0100

    Added unit tests for TimerTriggeredMessage

--- src/UnitTests/TimerTriggeredMessageTests/ShouldDisplayShould.cs
+++ src/UnitTests/TimerTriggeredMessageTests/ShouldDisplayShould.cs
@@ -1,5 +1,4 @@
 using System;
-using Essenbee.Bot.Core;
 using Essenbee.Bot.Core.Interfaces;
 using Essenbee.Bot.Core.Messaging;
 using FakeItEasy;
@@ -15,6 +14,7 @@ namespace UnitTests.TimerTriggeredMessageTests
         [TestMethod]
         public void ReturnFalse_WhenCreated()
         {
+            //Arrange
             var fakeClock = A.Fake<IClock>();
 
             A.CallTo(() => fakeClock.Now).Returns(new DateTime(2018, 6, 28, 12, 0, 0));
@@ -27,7 +27,89 @@ namespace UnitTests.TimerTriggeredMessageTests
 
             testMsg.Init(fakeClock.Now);
             
+            // Act and Assert
             Assert.IsFalse(testMsg.ShouldDisplay(fakeClock.Now));
+        }
+
+        [TestMethod]
+        public void ReturnFalse_AfterHalfDelayExpired()
+        {
+            //Arrange
+            var fakeClock = A.Fake<IClock>();
+
+            A.CallTo(() => fakeClock.Now).Returns(new DateTime(2018, 6, 28, 12, 0, 0));
+
+            var testMsg = new TimerTriggeredMessage
+            {
+                Delay = Delay,
+                Message = "Hi, this is a timed message from CoreBot!"
+            };
+
+            testMsg.Init(fakeClock.Now);
+
+            // Act and Assert
+            Assert.IsFalse(testMsg.ShouldDisplay(fakeClock.Now.AddSeconds(30)));
+        }
+
+        [TestMethod]
+        public void ReturnTrue_AfterDelayExpires()
+        {
+            // Arrange
+            var fakeClock = A.Fake<IClock>();
+
+            A.CallTo(() => fakeClock.Now).Returns(new DateTime(2018, 6, 28, 12, 0, 0));
+
+            var testMsg = new TimerTriggeredMessage
+            {
+                Delay = Delay,
+                Message = "Hi, this is a timed message from CoreBot!"
+            };
+
+            testMsg.Init(fakeClock.Now);
+
+            // Act and Assert
+            Assert.IsTrue(testMsg.ShouldDisplay(fakeClock.Now.AddMinutes(Delay)));
+        }
+
+        [TestMethod]
+        public void ReturnFalse_ImmediatelyAfterGetMessage()
+        {
+            // Arrange
+            var fakeClock = A.Fake<IClock>();
+
+            A.CallTo(() => fakeClock.Now).Returns(new DateTime(2018, 6, 28, 12, 0, 0));
+
+            var testMsg = new TimerTriggeredMessage
+            {
+                Delay = Delay,
+                Message = "Hi, this is a timed message from CoreBot!"
+            };
+
+            testMsg.Init(fakeClock.Now);
+            testMsg.GetMessage(fakeClock.Now.AddMinutes(Delay)); // Will reset the timer
+
+            // Act and Assert
+            Assert.IsFalse(testMsg.ShouldDisplay(fakeClock.Now.AddMinutes(Delay)));
+        }
+
+        [TestMethod]
+        public void ReturnTrue_AfterExpiryOfSecondDelay()
+        {
+            // Arrange
+            var fakeClock = A.Fake<IClock>();
+
+            A.CallTo(() => fakeClock.Now).Returns(new DateTime(2018, 6, 28, 12, 0, 0));
+
+            var testMsg = new TimerTriggeredMessage
+            {
+                Delay = Delay,
+                Message = "Hi, this is a timed message from CoreBot!"
+            };
+
+            testMsg.GetMessage(fakeClock.Now.AddMinutes(1));
+
+            // Act and Assert
+            Assert.IsTrue(testMsg.ShouldDisplay(fakeClock.Now.AddMinutes(2 * Delay)));
         }
     }
 }
