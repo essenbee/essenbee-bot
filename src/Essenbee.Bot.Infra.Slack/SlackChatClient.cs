94cc3386681e4e811643fe42c12fdad3289f9446
Stuart Bonham <sbonham@rics.org>
2018-06-28 15:59:07 +0100

    Fixed project references and added a stub SlackChatClient class

---
+++ src/Essenbee.Bot.Infra.Slack/SlackChatClient.cs
@@ -0,0 +1,13 @@
+﻿
+using Essenbee.Bot.Core.Interfaces;
+
+namespace Essenbee.Bot.Infra.Slack
+{
+    public class SlackChatClient: IChatClient
+    {
+        public void PostMessage(string channel, string text)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
