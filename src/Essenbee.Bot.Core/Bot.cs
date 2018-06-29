f058312c5b33e7713aac221ccaee6a8509b4c605
Stuart Bonham <sbonham@rics.org>
2018-06-29 15:17:35 +0100

    Added Bot class to Core and moved code into it from the console app

---
+++ src/Essenbee.Bot.Core/Bot.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Essenbee.Bot.Core.Interfaces;
+using Essenbee.Bot.Core.Messaging;
+using Essenbee.Bot.Core.Utilities;
+using static System.Console;
+
+namespace Essenbee.Bot.Core
+{
+    public class Bot
+    {
+        public List<IChatClient> ConnectedClients { get; }
+
+        private static bool _endProgram = false;
+
+        public Bot(List<IChatClient> connectedClients)
+        {
+            ConnectedClients = connectedClients;
+        }
+
+        public void Start()
+        {
+            Console.CancelKeyPress += OnCtrlC;
+            var defaultChannel = "general";
+
+            WriteLine();
+            WriteLine("{}oo((X))ΞΞΞΞΞΞΞΞΞΞΞΞΞ>");
+            WriteLine();
+            WriteLine("Press [Ctrl]+C to exit.");
+            WriteLine();
+
+            var autoMessaging = new AutoMessaging(new SystemClock());
+
+            var testMsg = new TimerTriggeredMessage
+            {
+                Delay = 1, // Minutes
+                Message = "Hi, this is a timed message from CoreBot!"
+            };
+
+            autoMessaging.PublishMessage(testMsg);
+
+            while (true)
+            {
+                Thread.Sleep(1000);
+
+                // Show Timer Triggered Messages
+                autoMessaging.EnqueueMessagesToDisplay();
+
+                while (true)
+                {
+                    var result = autoMessaging.DequeueNextMessage();
+
+                    if (!result.isMessage) break;
+
+                    foreach (var client in ConnectedClients)
+                    {
+                        var channelId = client.Channels.ContainsKey(defaultChannel) ?
+                            client.Channels[defaultChannel]
+                            : string.Empty;
+
+                        client.PostMessage(channelId,
+                            $"{DateTime.Now.ToShortTimeString()} - {result.message}");
+                    }
+                }
+
+                if (_endProgram) break;
+            }
+        }
+
+        private void OnCtrlC(object sender, ConsoleCancelEventArgs e)
+        {
+            foreach (var client in ConnectedClients)
+            {
+                client.Disconnect();
+            }
+
+            _endProgram = true;
+        }
+    }
+}
--- src/Essenbee.Bot/Program.cs
+++ src/Essenbee.Bot/Program.cs
@@ -17,9 +17,6 @@ namespace Essenbee.Bot
     {
         public static IConfiguration Configuration { get; set; }
 
-        private static IList<IChatClient> _connectedChatClients;
-        private static bool _endProgram = false;
-
         public static void Main(string[] args)
         {
             var devEnvironmentVariable = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
@@ -29,17 +26,13 @@ namespace Essenbee.Bot
                 .SetBasePath(Directory.GetCurrentDirectory())
                 .AddJsonFile("appsettings.json");
 
-
-            if (isDevelopment) // Only add secrets in development
+            // Only use secrets in Development; in Production, use appsettings.json
+            if (isDevelopment)
             {
                 builder.AddUserSecrets<UserSecrets>();
             }
 
             Configuration = builder.Build();
-
-            WriteLine("CoreBot is getting ready....");
-
-            Console.CancelKeyPress += OnCtrlC;
 
             // Make UserSecrets injectable ...
             var services = new ServiceCollection()
@@ -51,49 +44,11 @@ namespace Essenbee.Bot
             services.GetService<UserSecrets>();
 
             var secrets = services.GetService<UserSecretsProvider>().Secrets;
-            var defaultChannel = "general";
 
-            WriteLine("Press [Ctrl]+C to exit.");
+            var bot = new Core.Bot(ConnectChatClients(secrets));
 
-            var autoMessaging = new AutoMessaging(new SystemClock());
-
-            var testMsg = new TimerTriggeredMessage
-            {
-                Delay = 1, // Minutes
-                Message = "Hi, this is a timed message from CoreBot!"
-            };
-
-            autoMessaging.PublishMessage(testMsg);
-
-            // Handle multiple chat clients that implement IChatClient ...
-            _connectedChatClients = ConnectChatClients(secrets);
-
-            while (true)
-            {
-                Thread.Sleep(1000);
-
-                // Show Timer Triggered Messages
-                autoMessaging.EnqueueMessagesToDisplay();
-
-                while (true)
-                {
-                    var result = autoMessaging.DequeueNextMessage();
-
-                    if (!result.isMessage) break;
-
-                    foreach (var client in _connectedChatClients)
-                    {
-                        var channelId = client.Channels.ContainsKey(defaultChannel) ? 
-                            client.Channels[defaultChannel] 
-                            : string.Empty;
-
-                        client.PostMessage(channelId,
-                            $"{DateTime.Now.ToShortTimeString()} - {result.message}");
-                    }
-                }
-
-                if (_endProgram) break;
-            }
+            WriteLine("CoreBot is getting ready....");
+            bot.Start();
         }
 
         private static List<IChatClient> ConnectChatClients(UserSecrets secrets)
@@ -109,16 +64,6 @@ namespace Essenbee.Bot
             Thread.Sleep(5000);
 
             return connectedClients;
-        }
-
-        private static void OnCtrlC(object sender, ConsoleCancelEventArgs e)
-        {
-            foreach (var client in _connectedChatClients)
-            {
-                client.Disconnect();
-            }
-
-            _endProgram = true;
         }
     }
 }
