773b197e34d6e09af56d781dbaceaf787eb40354
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-06-30 17:22:43 +0100

    Updated to SlackLibCore 1.0.2 which has experiments ping-pong protocol enabled.

--- src/Essenbee.Bot.Infra.Slack/SlackChatClient.cs
+++ src/Essenbee.Bot.Infra.Slack/SlackChatClient.cs
@@ -24,6 +24,7 @@ namespace Essenbee.Bot.Infra.Slack
             _slackClient.ServiceConnectionFailed += OnDisconnected;
             _slackClient.ServiceDisconnected += OnDisconnected;
             _slackClient.Hello += OnHello;
+            _slackClient.PongReceived += OnPong;
 
             // Wire up additional events
             _slackClient.Message += OnMessage;
@@ -78,6 +79,12 @@ namespace Essenbee.Bot.Infra.Slack
             }
 
             WriteLine();
+        }
+
+        private void OnPong()
+        {
+            _connectionFailures = 0;
+            WriteLine("--> Pong received!");
         }
 
         private void OnDisconnected()
