9538ba2a1f687d2e6ca96443482a808a2babd66e
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-07-14 11:56:26 +0100

    Basic logging configured, more to do

--- src/Essenbee.Bot.Web/Program.cs
+++ src/Essenbee.Bot.Web/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using System;
 
 namespace Essenbee.Bot.Web
 {
@@ -12,13 +14,22 @@ namespace Essenbee.Bot.Web
 
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                //.ReadFrom.Configuration(Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.RollingFile(@"C:\logs\botlog-.txt",
+                                     fileSizeLimitBytes: 1_000_000,
+                                     flushToDiskInterval: TimeSpan.FromSeconds(1),
+                                     shared: true)
+                .CreateLogger();
+
+            Log.Information("Starting web host...");
             var host = CreateWebHostBuilder(args).Build();
 
             using (var scope = host.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 var context = services.GetRequiredService<AppDataContext>();
-                // context.Database.EnsureCreated();
                 context.Database.Migrate();
             }
 
@@ -26,7 +37,8 @@ namespace Essenbee.Bot.Web
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .UseSerilog()
+            .UseStartup<Startup>();
 
     }
 }
