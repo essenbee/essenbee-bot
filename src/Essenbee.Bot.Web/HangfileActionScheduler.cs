bd206e869e65f1fe0b461be0b9a5f281e4653cee
Stuart Bonham <sbonham@rics.org>
2018-07-20 17:42:53 +0100

    Added implementation of IActionScheduler for Hangfire server

---
+++ src/Essenbee.Bot.Web/HangfileActionScheduler.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Essenbee.Bot.Core.Interfaces;
+using Essenbee.Bot.Core.Messaging;
+using Hangfire;
+using Serilog;
+
+namespace Essenbee.Bot.Web
+{
+    public class HangfileActionScheduler : IActionScheduler
+    {
+        private readonly IList<IChatClient> _chatClients;
+        
+        public HangfileActionScheduler(IList<IChatClient> chatClients)
+        {
+            _chatClients = chatClients;
+        }
+
+        public void AddAction(IScheduledAction action)
+        {
+            Log.Information($"Scheduling {action.Name}.with Hangfire server...");
+
+            switch (action)
+            {
+               case DelayedMessage delayedMsg:
+                    var msg = delayedMsg.Message;
+                    var chnl = delayedMsg.Channel;
+                    BackgroundJob.Schedule(() => _chatClients.Single().PostMessage(chnl, msg), delayedMsg.Delay);
+                    break;
+
+                case RepeatingMessage repeatingMsg:
+                    var message = repeatingMsg.Message;
+                    var channel = repeatingMsg.Channel;
+                    RecurringJob.AddOrUpdate(
+                        repeatingMsg.Name,
+                        () => _chatClients.Single().PostMessage(channel, message),
+                        Cron.MinuteInterval(repeatingMsg.IntervalInMinutes));
+                    break;
+            }
+        }
+    }
+}
