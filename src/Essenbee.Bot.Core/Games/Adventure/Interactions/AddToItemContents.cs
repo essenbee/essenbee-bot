2d75b82963ade93ec9835eccd6e72f86041f9872
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-08-26 11:52:43 +0100

    Added AdToItemContents interaction

---
+++ src/Essenbee.Bot.Core/Games/Adventure/Interactions/AddToItemContents.cs
@@ -0,0 +1,18 @@
+﻿namespace Essenbee.Bot.Core.Games.Adventure.Interactions
+{
+    public class AddToItemContents
+    {
+        private readonly AdventureItem _itemToAdd;
+
+        public AddToItemContents(AdventureItem itemToAdd)
+        {
+            _itemToAdd = itemToAdd;
+        }
+
+        public bool Do(AdventurePlayer player, AdventureItem item)
+        {
+            item.Contents.Add(_itemToAdd);
+            return true;
+        }
+    }
+}
