229634cd8b2f883789a2012a9626aa7c17b9b654
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-08-26 20:27:14 +0100

    Update to Grate item

--- src/Essenbee.Bot.Core/Games/Adventure/Items/Grate.cs
+++ src/Essenbee.Bot.Core/Games/Adventure/Items/Grate.cs
@@ -19,16 +19,22 @@ namespace Essenbee.Bot.Core.Games.Adventure.Items
             open.RegisteredInteractions.Add(new Open());
             Interactions.Add(open);
 
+            var found = Game.TryGetLocation("depression", out var depression);
+
             var unlock = new ItemInteraction(Game, "unlock");
             unlock.RegisteredInteractions.Add(new Unlock());
             unlock.RegisteredInteractions.Add(new Display("You open the grate and see a dark space below it. A rusty iron ladder leads down into pitch blackness!"));
-            unlock.RegisteredInteractions.Add(new AddMoves(new Dictionary<string, string> 
-            {
-                { "down", "cave1" },
-                { "d", "cave1" },
-            }));
 
-            var found = Game.TryGetLocation("cave1", out var entranceCave);
+            if (found)
+            {
+                unlock.RegisteredInteractions.Add(new AddMoves(new Dictionary<string, string>
+                {
+                    { "down", "cave1" },
+                    { "d", "cave1" },
+                }, depression));
+            }
+
+            found = Game.TryGetLocation("cave1", out var entranceCave);
 
             if (found)
             {
