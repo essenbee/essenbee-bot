17b91d52bba281c45d368f18799d057455e27508
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-08-31 23:39:43 +0100

    Start to add abillity to drink water

--- src/Essenbee.Bot.Core/Games/Adventure/Items/PintOfWater.cs
+++ src/Essenbee.Bot.Core/Games/Adventure/Items/PintOfWater.cs
@@ -1,4 +1,6 @@
-﻿using Essenbee.Bot.Core.Games.Adventure.Interfaces;
+﻿using Essenbee.Bot.Core.Games.Adventure.Interactions;
+using Essenbee.Bot.Core.Games.Adventure.Interfaces;
+using System.Linq;
 
 namespace Essenbee.Bot.Core.Games.Adventure.Items
 {
@@ -11,6 +13,52 @@ namespace Essenbee.Bot.Core.Games.Adventure.Items
             PluralName = "pin of *water*";
             IsPortable = false;
             IsEndlessSupply = true;
+            MustBeContainedIn = Item.Bottle;
+
+            var drink = new ItemInteraction(Game, "drink");
+            Interactions.Add(drink);
+        }
+
+        public override bool Interact(string verb, AdventurePlayer player)
+        {
+            verb = verb.ToLower();
+            var interaction = Interactions.FirstOrDefault(c => c.IsMatch(verb) && c.ShouldExecute());
+
+            if (interaction != null)
+            {
+                if (interaction.Verbs.Contains("drink"))
+                {
+                    if (!player.CurrentLocation.WaterPresent)
+                    {
+                        if (player.Inventory.GetItems().Any(i => i.ItemId.Equals(Item.Bottle)))
+                        {
+                            var bottle = player.Inventory.GetItems().First(i => i.ItemId.Equals(Item.Bottle));
+                            if (bottle.Contents.Any(w => w.ItemId.Equals(Item.PintOfWater)))
+                            {
+                                var water = bottle.Contents.Where(i => i.ItemId.Equals(Item.PintOfWater));
+                                interaction.RegisteredInteractions.Add(new RemoveFromInventory());
+                            }
+                        }
+                        else
+                        {
+                            var msg = new Display("There is no water here!");
+                            msg.Do(player);
+                            return true;
+                        }
+                    }
+
+                    interaction.RegisteredInteractions.Add(new Display("Mmmm, refreshing!"));
+                }
+
+                foreach (var action in interaction.RegisteredInteractions)
+                {
+                    action.Do(player, this);
+                }
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
