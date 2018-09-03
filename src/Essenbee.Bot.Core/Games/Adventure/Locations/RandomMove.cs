88ec80a21f5667686c78be0849ddc0eac855cdab
Stuart Bonham <sbonham@rics.org>
2018-09-03 13:07:54 +0100

    Added RandomMove

--- src/Essenbee.Bot.Core/Games/Adventure/Locations/RandomMove.cs
+++ src/Essenbee.Bot.Core/Games/Adventure/Locations/RandomMove.cs
@@ -1,4 +1,5 @@
-﻿using Essenbee.Bot.Core.Games.Adventure.Interfaces;
+﻿using System;
+using Essenbee.Bot.Core.Games.Adventure.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -6,10 +7,12 @@ namespace Essenbee.Bot.Core.Games.Adventure.Locations
 {
     public class RandomMove : IPlayerMove
     {
-        public Location Destination { get; }
+        public Location Destination => GetRandomLocation();
         public List<string> Moves { get; }
         public string MoveText { get; set; }
         private IList<Location> _possibleDestinations;
+
+        private static readonly Random getRandom = new Random();
 
         public RandomMove(string moveText, IList<Location> possibleDestinations, params string[] moves)
         { 
@@ -19,5 +22,19 @@ namespace Essenbee.Bot.Core.Games.Adventure.Locations
         }
 
         public bool IsMatch(string move) => Moves.Any(v => move.Equals(v));
+
+        private Location GetRandomLocation()
+        {
+            var loc = GetRandomNumber(0, _possibleDestinations.Count - 1);
+            return _possibleDestinations[loc];
+        }
+
+        private int GetRandomNumber(int min, int max)
+        {
+            lock (getRandom) // synchronize
+            {
+                return getRandom.Next(min, max);
+            }
+        }
     }
 }
