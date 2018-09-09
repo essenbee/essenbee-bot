5fe873799bcc642aa1b5bc9e18e01c3c9f9d7bd2
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-09-09 19:06:39 +0100

    Added Open tests

---
+++ src/UnitTests/AdventureGameTests/OpenShould.cs
@@ -0,0 +1,80 @@
+﻿using Essenbee.Bot.Core.Games.Adventure.Interactions;
+using Essenbee.Bot.Core.Games.Adventure.Interfaces;
+using Essenbee.Bot.Core.Games.Adventure.Items;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace UnitTests.AdventureGameTests
+{
+    [TestClass]
+    public class OpenShould
+    {
+        [TestMethod]
+        public void SetItemToOpen_GivenItIsClosedAndNotLocked()
+        {
+            // Arrange
+            var fakePlayer = A.Fake<IAdventurePlayer>();
+            var fakeGame = A.Fake<IReadonlyAdventureGame>();
+
+            var fakeCage = A.Fake<IAdventureItem>();
+            A.CallTo(() => fakeCage.ItemId).Returns(Item.Cage);
+            A.CallTo(() => fakeCage.IsPortable).Returns(true);
+            A.CallTo(() => fakeCage.IsMatch("cage")).Returns(true);
+            fakeCage.IsOpen = false;
+
+            // Act
+            var action = new Open();
+            var result = action.Do(fakePlayer, fakeCage);
+
+            // Assert
+            Assert.IsTrue(fakeCage.IsOpen);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void DoNothing_GivenItemIsLocked()
+        {
+            // Arrange
+            var fakePlayer = A.Fake<IAdventurePlayer>();
+            var fakeGame = A.Fake<IReadonlyAdventureGame>();
+
+            var fakeCage = A.Fake<IAdventureItem>();
+            A.CallTo(() => fakeCage.ItemId).Returns(Item.Cage);
+            A.CallTo(() => fakeCage.IsPortable).Returns(true);
+            A.CallTo(() => fakeCage.IsMatch("cage")).Returns(true);
+            fakeCage.IsOpen = false;
+            fakeCage.IsLocked = true;
+
+            // Act
+            var action = new Open();
+            var result = action.Do(fakePlayer, fakeCage);
+
+            // Assert
+            Assert.IsFalse(fakeCage.IsOpen);
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void DoNothing_GivenItemIsAlreadyOpen()
+        {
+            // Arrange
+            var fakePlayer = A.Fake<IAdventurePlayer>();
+            var fakeGame = A.Fake<IReadonlyAdventureGame>();
+
+            var fakeCage = A.Fake<IAdventureItem>();
+            A.CallTo(() => fakeCage.ItemId).Returns(Item.Cage);
+            A.CallTo(() => fakeCage.IsPortable).Returns(true);
+            A.CallTo(() => fakeCage.IsMatch("cage")).Returns(true);
+            fakeCage.IsOpen = true;
+
+            // Act
+            var action = new Open();
+            var result = action.Do(fakePlayer, fakeCage);
+
+            // Assert
+            Assert.IsTrue(fakeCage.IsOpen);
+            Assert.IsFalse(result);
+        }
+    }
+}
