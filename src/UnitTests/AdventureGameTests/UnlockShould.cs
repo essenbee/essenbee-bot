d505bfbd73d97d41c182a1ef8506f53cd9d555b1
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-09-09 19:26:22 +0100

    Added Unlock tests

--- src/UnitTests/AdventureGameTests/UnlockShould.cs
+++ src/UnitTests/AdventureGameTests/UnlockShould.cs
@@ -1,41 +1,91 @@
-﻿using Essenbee.Bot.Core.Games.Adventure.Interactions;
+﻿using Essenbee.Bot.Core.Games.Adventure;
+using Essenbee.Bot.Core.Games.Adventure.Interactions;
 using Essenbee.Bot.Core.Games.Adventure.Interfaces;
 using Essenbee.Bot.Core.Games.Adventure.Items;
 using FakeItEasy;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace UnitTests.AdventureGameTests
 {
     [TestClass]
-    public class OpenShould
+    public class UnlockShould
     {
         [TestMethod]
-        public void SetItemToOpen_GivenItIsClosedAndNotLocked()
+        public void SetItemToUnlocked_GivenItIsLockedAndPlayerHasKey()
         {
             // Arrange
+            var inventory = new Inventory();
+
             var fakePlayer = A.Fake<IAdventurePlayer>();
+            A.CallTo(() => fakePlayer.Inventory).Returns(inventory);
             var fakeGame = A.Fake<IReadonlyAdventureGame>();
+
+            var fakeKey = A.Fake<IAdventureItem>();
+            A.CallTo(() => fakeKey.ItemId).Returns(Item.Key);
+            A.CallTo(() => fakeKey.Nouns).Returns(new List<string> { "key" });
+            A.CallTo(() => fakeKey.IsPortable).Returns(true);
+            A.CallTo(() => fakeKey.IsMatch("key")).Returns(true);
+            inventory.AddItem(fakeKey);
 
             var fakeCage = A.Fake<IAdventureItem>();
             A.CallTo(() => fakeCage.ItemId).Returns(Item.Cage);
             A.CallTo(() => fakeCage.IsPortable).Returns(true);
             A.CallTo(() => fakeCage.IsMatch("cage")).Returns(true);
             fakeCage.IsOpen = false;
+            fakeCage.IsLocked = true;
+            fakeCage.ItemIdToUnlock = Item.Key;
 
             // Act
-            var action = new Open();
+            var action = new Unlock();
             var result = action.Do(fakePlayer, fakeCage);
 
             // Assert
-            Assert.IsTrue(fakeCage.IsOpen);
+            Assert.IsFalse(fakeCage.IsLocked);
             Assert.IsTrue(result);
         }
 
         [TestMethod]
-        public void DoNothing_GivenItemIsLocked()
+        public void DoNothing_GivenItIsUnklocked()
         {
             // Arrange
+            var inventory = new Inventory();
+
             var fakePlayer = A.Fake<IAdventurePlayer>();
+            A.CallTo(() => fakePlayer.Inventory).Returns(inventory);
+            var fakeGame = A.Fake<IReadonlyAdventureGame>();
+
+            var fakeKey = A.Fake<IAdventureItem>();
+            A.CallTo(() => fakeKey.ItemId).Returns(Item.Key);
+            A.CallTo(() => fakeKey.Nouns).Returns(new List<string> { "key" });
+            A.CallTo(() => fakeKey.IsPortable).Returns(true);
+            A.CallTo(() => fakeKey.IsMatch("key")).Returns(true);
+            inventory.AddItem(fakeKey);
+
+            var fakeCage = A.Fake<IAdventureItem>();
+            A.CallTo(() => fakeCage.ItemId).Returns(Item.Cage);
+            A.CallTo(() => fakeCage.IsPortable).Returns(true);
+            A.CallTo(() => fakeCage.IsMatch("cage")).Returns(true);
+            fakeCage.IsOpen = false;
+            fakeCage.IsLocked = false;
+            fakeCage.ItemIdToUnlock = Item.Key;
+
+            // Act
+            var action = new Unlock();
+            var result = action.Do(fakePlayer, fakeCage);
+
+            // Assert
+            Assert.IsFalse(fakeCage.IsLocked);
+            Assert.IsFalse(result);
+        }
+        [TestMethod]
+        public void DoNothing_GivenPlayerHasNoKey()
+        {
+            // Arrange
+            var inventory = new Inventory();
+
+            var fakePlayer = A.Fake<IAdventurePlayer>();
+            A.CallTo(() => fakePlayer.Inventory).Returns(inventory);
             var fakeGame = A.Fake<IReadonlyAdventureGame>();
 
             var fakeCage = A.Fake<IAdventureItem>();
@@ -44,35 +94,14 @@ namespace UnitTests.AdventureGameTests
             A.CallTo(() => fakeCage.IsMatch("cage")).Returns(true);
             fakeCage.IsOpen = false;
             fakeCage.IsLocked = true;
+            fakeCage.ItemIdToUnlock = Item.Key;
 
             // Act
-            var action = new Open();
+            var action = new Unlock();
             var result = action.Do(fakePlayer, fakeCage);
 
             // Assert
-            Assert.IsFalse(fakeCage.IsOpen);
-            Assert.IsFalse(result);
-        }
-
-        [TestMethod]
-        public void DoNothing_GivenItemIsAlreadyOpen()
-        {
-            // Arrange
-            var fakePlayer = A.Fake<IAdventurePlayer>();
-            var fakeGame = A.Fake<IReadonlyAdventureGame>();
-
-            var fakeCage = A.Fake<IAdventureItem>();
-            A.CallTo(() => fakeCage.ItemId).Returns(Item.Cage);
-            A.CallTo(() => fakeCage.IsPortable).Returns(true);
-            A.CallTo(() => fakeCage.IsMatch("cage")).Returns(true);
-            fakeCage.IsOpen = true;
-
-            // Act
-            var action = new Open();
-            var result = action.Do(fakePlayer, fakeCage);
-
-            // Assert
-            Assert.IsTrue(fakeCage.IsOpen);
+            Assert.IsTrue(fakeCage.IsLocked);
             Assert.IsFalse(result);
         }
     }
