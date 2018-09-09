a83cdc549bc686fa424933a87dd5ab5d177e9273
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-09-09 19:29:57 +0100

    Added test

---
+++ src/UnitTests/AdventureGameTests/UpdateItemNameShould.cs
@@ -0,0 +1,37 @@
+﻿using Essenbee.Bot.Core.Games.Adventure.Interactions;
+using Essenbee.Bot.Core.Games.Adventure.Interfaces;
+using Essenbee.Bot.Core.Games.Adventure.Items;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTests.AdventureGameTests
+{
+    [TestClass]
+    public class UpdateItemNameShould
+    {
+        [TestMethod]
+        public void ChangeItemName()
+        {
+            // Arrange
+            var fakePlayer = A.Fake<IAdventurePlayer>();
+            var fakeGame = A.Fake<IReadonlyAdventureGame>();
+
+            var fakeLamp = A.Fake<IAdventureItem>();
+            A.CallTo(() => fakeLamp.ItemId).Returns(Item.Lamp);
+            A.CallTo(() => fakeLamp.Nouns).Returns(new List<string> { "lamp" });
+            A.CallTo(() => fakeLamp.IsEndlessSupply).Returns(false);
+            A.CallTo(() => fakeLamp.IsPortable).Returns(true);
+            A.CallTo(() => fakeLamp.IsMatch("lamp")).Returns(true);
+            fakeLamp.Name = "lamp";
+
+            // Act
+            var action = new UpdateItemName("smashed lamp");
+            var result = action.Do(fakePlayer, fakeLamp);
+
+            // Assert
+            Assert.AreEqual("smashed lamp", fakeLamp.Name);
+            Assert.IsTrue(result);
+        }
+    }
+}
