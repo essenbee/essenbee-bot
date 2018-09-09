46c11f8b945bcd95170caab98cd8ccb9cc6f9b7d
Stu Bonham <stuart_bonham@blueyonder.co.uk>
2018-09-09 18:41:14 +0100

    Added ActivateItem test

---
+++ src/UnitTests/AdventureGameTests/ActivateItemShould.cs
@@ -0,0 +1,35 @@
+﻿using Essenbee.Bot.Core.Games.Adventure.Interactions;
+using Essenbee.Bot.Core.Games.Adventure.Interfaces;
+using Essenbee.Bot.Core.Games.Adventure.Items;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTests.AdventureGameTests
+{
+    [TestClass]
+    public class ActivateItemShould
+    {
+        [TestMethod]
+        public void SetItemToActive()
+        {
+            // Arrange
+            var fakePlayer = A.Fake<IAdventurePlayer>();
+            var fakeGame = A.Fake<IReadonlyAdventureGame>();
+
+            var fakeLamp = A.Fake<IAdventureItem>();
+            A.CallTo(() => fakeLamp.ItemId).Returns(Item.Lamp);
+            A.CallTo(() => fakeLamp.Nouns).Returns(new List<string> { "lamp" });
+            A.CallTo(() => fakeLamp.IsEndlessSupply).Returns(false);
+            A.CallTo(() => fakeLamp.IsPortable).Returns(true);
+            A.CallTo(() => fakeLamp.IsMatch("lamp")).Returns(true);
+
+            // Act
+            var action = new ActivateItem(string.Empty);
+            action.Do(fakePlayer, fakeLamp);
+
+            // Assert
+            Assert.IsTrue(fakeLamp.IsActive);
+        }
+    }
+}
